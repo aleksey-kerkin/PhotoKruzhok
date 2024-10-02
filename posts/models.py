2b29171b14889cf4eb4b6d15586e09c8e3d2eca2
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-10-02 15:55:38 +0500

    Add post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,39 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to="posts/images/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(
+        max_length=255, unique_for_date="created_at", editable=False
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user.username}'s post at {self.created_at.strftime('%Y-%m-%d %H:%M')}"  # noqa: E501
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.text:
+            self.slug = slugify(self.text[:50])
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:post_detail",
+            args=[
+                self.created_at.year,
+                self.created_at.month,
+                self.created_at.day,
+                self.slug,
+            ],
+        )
